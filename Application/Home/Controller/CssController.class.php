cd8f5a5208eeef7610237a6fbcf54dc62219eb44
xhq <1434970057@qq.com>
2015-07-23 00:34:17 +0800

    css

---
+++ Application/Home/Controller/CssController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+class CssController extends Controller {
+	public function __contruct() {
+
+	}
+	public function css() {
+		$this->display();
+	}
+}
---
+++ Application/Runtime/Cache/Home/6c308de4b8d8782253c82fbbc5923ac6.php
@@ -0,0 +1,26 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<title>css</title>
+	<link rel="stylesheet" href="/tp323c/Public/css/css.css">
+</head>
+<body>
+<div class="nav">
+	<ul class="nav">
+		<li class=""><a href="#" class="active">博客</a></li>
+		<li><a href="#">Css3</a></li>
+		<li><a href="#">Mobile</a></li>
+		<li><a href="#">Sass</a></li>
+		<li><a href="#">Responsive</a></li>
+		<li><a href="#">SVG</a></li>
+		<li><a href="#">译文</a></li>
+		<li><a href="#">在线教程</a></li>
+		<li><a href="#">资源</a></li>
+		<li><a href="#">案例</a></li>
+		<li><a href="#">工具</a></li>
+		<li><a href="#">周刊</a></li>
+		<li><a href="#">标签云</a></li>
+	</ul>
+</div>
+</body>
+</html>
